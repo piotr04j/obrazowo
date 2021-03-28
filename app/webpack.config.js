922ba833459f6283ebd10f7d9f16b2cf1fad7c00
piotr04j <darricck04@gmail.com>
2021-03-28 16:31:42 +0200

    some fixes around loading scripts

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,20 +1,24 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
+const WebpackAssetsManifest = require('webpack-assets-manifest')
 
 module.exports = (env, argv) => {
   return  {
     entry: { main: './frontend/scripts.js'},
     output: {
       path: path.resolve(__dirname, 'public/dist'),
-      filename: '[contenthash].bundle.js',
+      filename: '[name]-[hash].js',
+      chunkFilename: '[id]-[chunkhash].js'
     },
     devtool: "inline-source-map",
     plugins: [
       new CleanWebpackPlugin(),
-      new WebpackManifestPlugin({
-        writeToFileEmit: true,
-        fileName: 'entrypoints.json'
+      new WebpackAssetsManifest({
+        writeToDisk: true,
+        entrypoints: true,
+        output: 'entrypoints.json',
+        publicPath : 'dist/',
+        assets: {}
       })
     ],
     resolve: {
@@ -22,7 +26,15 @@ module.exports = (env, argv) => {
     },
     module: {
       rules: [
-        { test: /\.(js|tsx?)$/, loader: "ts-loader" }
+        { test: /\.(js|tsx?)$/, loader: "ts-loader" },
+        {
+          test: /\.scss$/i,
+          use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
       ]
     }
   }
