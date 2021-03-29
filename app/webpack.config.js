833db34a5b95f14213493b2d2e7ffae6fb184c6e
piotr04j <darricck04@gmail.com>
2021-03-29 18:52:07 +0200

    improved some configuration of webpack

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,24 +1,30 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const WebpackAssetsManifest = require('webpack-assets-manifest')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = (env, argv) => {
+  const devMode = argv.mode !== 'production'
   return  {
-    entry: { main: './frontend/scripts.js'},
+    entry: { main: './frontend/app.js'},
     output: {
       path: path.resolve(__dirname, 'public/dist'),
-      filename: '[name]-[hash].js',
-      chunkFilename: '[id]-[chunkhash].js'
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[name].[chunkhash].js'
     },
-    devtool: "inline-source-map",
+    devtool: devMode && "inline-source-map",
     plugins: [
       new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: devMode ? '[name].css' : '[name].[chunkhash].css',
+        chunkFilename: devMode ? '[name].css' : '[name].[chunkhash].css'
+      }),
       new WebpackAssetsManifest({
         writeToDisk: true,
         entrypoints: true,
         output: 'entrypoints.json',
-        publicPath : 'dist/',
-        assets: {}
+        publicPath : 'dist/'
       })
     ],
     resolve: {
@@ -28,14 +34,26 @@ module.exports = (env, argv) => {
       rules: [
         { test: /\.(js|tsx?)$/, loader: "ts-loader" },
         {
-          test: /\.scss$/i,
+          test: /\.s?css$/i,
           use: [
-            "style-loader",
-            "css-loader",
-            "sass-loader",
+            { loader: 'css-loader', options: { sourceMap: true } },
+            { loader: 'sass-loader', options: { sourceMap: true } }
           ],
         },
       ]
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new CssMinimizerPlugin(),
+      ],
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'public/dist'),
+      compress: true,
+      port: 8003,
+      hot: true,
+      writeToDisk: true
     }
   }
 }
