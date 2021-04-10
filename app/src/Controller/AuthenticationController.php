a82621641749117a16d81880462a1c42d396801d
piotr04j <darricck04@gmail.com>
2021-04-10 17:15:13 +0200

    fixed controller name

--- app/src/Controller/AuthenticationController.php
+++ app/src/Controller/AuthenticationController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class AuthenticatorController extends AbstractController
+class AuthenticationController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login")
