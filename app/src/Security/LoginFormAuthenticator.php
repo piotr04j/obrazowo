0bcc240f4cafa60d75911e09fda7c7b138ec73a3
piotr04j <darricck04@gmail.com>
2021-04-13 17:25:58 +0200

    fixed redirect after success login

--- app/src/Security/LoginFormAuthenticator.php
+++ app/src/Security/LoginFormAuthenticator.php
@@ -96,7 +96,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-         return new RedirectResponse($this->urlGenerator->generate('/'));
+         return new RedirectResponse($this->urlGenerator->generate('homepage'));
     }
 
     protected function getLoginUrl()
